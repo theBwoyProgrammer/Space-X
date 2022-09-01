8f514a3b156a37c7eb606bbaf06f53e1d3d28613
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-09-01 18:37:18 +0300

    badge reserve and cancel

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const FETCH_ROCKET = 'rockets/rocket/FETCH_ROCKET';
+const RESERVED_ROCKET = 'rockets/rocket/RESERVED_ROCKET';
+const CANCELLED_RESERVE = 'rockets/rocket/CANCELLED_RESERVE';
 const BASE_URL = 'https://api.spacexdata.com/v3/rockets';
 const initialState = [];
 
@@ -13,10 +15,11 @@ export const getRockets = createAsyncThunk(
     response.map((res) => {
       arr.push(
         {
-          id: res.id,
+          id: res.rocket_id,
           name: res.rocket_name,
           description: res.description,
           image: res.flickr_images[0],
+          reserved: false,
         },
       );
       return res;
@@ -29,8 +32,33 @@ const rockets = (state = initialState, action) => {
   switch (action.type) {
     case `${FETCH_ROCKET}/fulfilled`:
       return action.payload;
+    case RESERVED_ROCKET:
+      return state.map((state) => {
+        if (state.id !== action.id) {
+          return state;
+        }
+        return { ...state, reserved: true };
+      });
+    case CANCELLED_RESERVE: {
+      return state.map((state) => {
+        if (state.id !== action.id) {
+          return state;
+        }
+        return { ...state, reserved: false };
+      });
+    }
     default: return state;
   }
 };
+
+export const reservedRocket = (id) => ({
+  type: RESERVED_ROCKET,
+  id,
+});
+
+export const cancelledReserve = (id) => ({
+  type: CANCELLED_RESERVE,
+  id,
+});
 
 export default rockets;
